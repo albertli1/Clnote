d6662bd8ac1d0a25850928ec60cabe376e612990
licong <congl1@21cn.com>
2016-09-27 16:51:20 +0800

    checkver

--- monkeyApp/src/main/java/apifunc.php
+++ monkeyApp/src/main/java/apifunc.php
@@ -7,12 +7,13 @@
  */
 
 
+
 // json xml plain html
 function setheader($type) {
 	// $type = "json"; 
-	$headstr4 = 'Content-Type:text/'.$type.';charset=utf-8';
+	$headstr = 'Content-Type:text/'.$type.';charset=utf-8';
 	// header('Content-Type:text/json;charset=utf-8');
-	header($headstr4);
+	header($headstr);
 }
 
 
@@ -30,20 +31,21 @@ function parser_action(){
 
 // $FFabcd = '组合变量';
 // $a = 'abcd';
-
 // $c = 'FF'.$a;
 // echo ${$c};
-
 // echo count(${'_GET'});
 // $action_method = '$_'.$reqmethod."['action']";
-
 // $a ='$_GET';
-
 //  echo ${$a}['action'];
-	$action_checkver = array("Function"=>array("name"=>"checkver","paramcount"=>"1","version"=>"0.1"));
-	$action_login = array("Function"=>array("name"=>"checkver","paramcount"=>"1","version"=>"0.1"));
-	$action_logout = array("Function"=>array("name"=>"checkver","paramcount"=>"1","version"=>"0.1"));
-	$action_allowlocation = array("Function"=>array("name"=>"checkver","paramcount"=>"1","version"=>"0.1"));
+
+
+	//@param1:client_version 	return:current_version
+	$action_checkver = array("Function"=>array("name"=>"checkver","paramcount"=>1,"version"=>0.01));
+	$action_login = array("Function"=>array("name"=>"login","paramcount"=>2,"version"=>0.01));
+	$action_logout = array("Function"=>array("name"=>"logout","paramcount"=>1,"version"=>0.01));
+	$action_getuserinfo = array("Function"=>array("name"=>"getuserinfo","paramcount"=>1,"version"=>0.01));
+	$action_getlocationtab = array("Function"=>array("name"=>"getlocationtab","paramcount"=>1,"version"=>0.01));
+	$action_allowlocation = array("Function"=>array("name"=>"allowlocation","paramcount"=>1,"version"=>0.01));
 	
 
 
@@ -54,18 +56,21 @@ function parser_action(){
 	// } else {
 	// 	exit("not support method");
 	// }
-
+	
+	global $gaction_method;
 	switch($_SERVER['REQUEST_METHOD'])
 	{
-		case 'GET': $action_method = $_GET; break;
-		case 'POST': $action_method = $_POST; break;
+		case 'GET': $gaction_method = $_GET; break;
+		case 'POST': $gaction_method = $_POST; break;
 		default:exit("not support method");
 	}
 
-	$action =  $action_method['action'];
-	if(isset(${$action}))
-		echo json_encode( ${$action});
-	else {
+
+	$action =  strtolower($gaction_method['action']);
+	if(isset(${$action})) {
+		// echo json_encode( ${$action});
+		return  ${$action};
+	} else {
 		$error_action = array("Errorn"=>"1","Msg"=>"Action: (". $action . ") not found!");
 		$error_resp = array("ErrorResp"=>$error_action);
 		error_report($error_resp);
@@ -80,10 +85,64 @@ function parser_action(){
 	
 }
 
+//function list
 
-function process_data() {
-	// echo "process_data";
+function checkver($action) {
+	// $result = 
+	// return $result;
+	global $gaction_method;
+	$paramcount = $action['Function']['paramcount'];
+	$version = $action['Function']['version'];
+
+	$reqver=floatval($gaction_method['version']);
+	$webver=floatval($version);
+
+	if($webver > $reqver) {
+		$result = array("Result"=>array("version" =>$version,"forceupgrade"=>true,"url"=>"www.localhost.com"));
+		echo json_encode($result);
+	} else {
+		$result = array("Result"=>array("version" =>$version,"forceupgrade"=>false,"url"=>"www.localhost.com"));
+		echo json_encode($result);
+	}
 	
+	 // echo json_encode($gaction_method);
+	 // echo json_encode($gaction_method);
+	// echo "1";
+}
+
+
+
+
+
+
+
+
+
+function process_data($action) {
+	// echo "process_data";
+	// echo json_encode( $action);
+
+	// switch (strtolower($action['Function']['name'])) {
+	// 	case 'checkver':
+	// 		# code...
+	// 		return checkver($action);
+	// 		break;
+		
+		// default:
+			# code...
+	 // echo $action['Function']['name'];
+	$realfunction = $action['Function']['name'];
+	if(isset($realfunction)) {
+		 echo $realfunction($action);
+	} else {
+		$error_action = array("Errorn"=>"2","Msg"=>"Action: (". $action . ") not process!");
+		$error_resp = array("ErrorResp"=>$error_action);
+		error_report($error_resp);
+	}
+
+		
+			// break;
+	// }
 	// actionlogic();
 }
 
@@ -105,13 +164,11 @@ function result_json($array) {
 	echo json_encode($result_array);
 }
 
-
-
 //run
 
 setheader("json");
-parser_action();
-process_data();
+$actions = parser_action();
+process_data($actions);
 output_result();
 	
 
